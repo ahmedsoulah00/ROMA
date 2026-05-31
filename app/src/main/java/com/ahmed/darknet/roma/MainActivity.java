db73fbff2201ba6a6b4cdbadc6bb080312efafde
ahmedsoulah00 <ahmadsoulah00@gmail.com>
2026-05-31 23:16:31 +0100

    Create MainActivity.java

---
+++ app/src/main/java/com/ahmed/darknet/roma/MainActivity.java
@@ -0,0 +1,12 @@
+package com.ahmed.darknet.roma;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // تم البناء بنجاح
+    }
+}
